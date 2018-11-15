4cbad92c77329f25c53fdf606c37f3007b5da56d
Steven Douglas <steven@douglas-enterprises.com>
2018-11-15 09:44:20 -0800

    multiple changes

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# Pull latest
-#git pull origin master
-
-# Update
-vim -c VundleUpdate -c quitall
+ORIGINAL_WD=$(pwd)
+cd ~/.vim
+timeout 2 git remote update > /dev/null 2>&1
+UPDATE_STATUS=$(git status -uno | grep -c -i 'behind\|different')
+if [ "$UPDATE_STATUS" != "0" ]; then
+	read -p "Updates available, update now? " -n 1 -r
+	echo
+	git reset --hard
+	git pull -f origin master
+	sh ~/.vim/setup.sh
+	echo "Updates complete."
+fi
+cd ${ORIGINAL_WD}
